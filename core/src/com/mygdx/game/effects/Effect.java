1f7bbf2c5f7f2f37d6355b4a4cadc9aa631bf9bb
Whizzpered <Whizzpered@Whizza.Dlink>
2015-11-16 19:49:51 +0300

    Merge origin/master
    Conflicts:
    	core/src/com/mygdx/game/GameStage.java
    	core/src/com/mygdx/game/effects/Effect.java

--- core/src/com/mygdx/game/effects/Effect.java
+++ core/src/com/mygdx/game/effects/Effect.java
@@ -13,31 +13,38 @@ import com.mygdx.game.objects.Entity;
  * @author Whizzpered
  */
 public class Effect {
+
     protected long time;
     protected int listPosition;
     private String name;
     protected GameStage stage;
+
     public String getName() {
         return name;
     }
 
-    public Effect(long time,String name) {
-        this.name=name;
-        this.time=time;
+    public Effect(long time, String name) {
+        this.name = name;
+        this.time = time;
     }
-    public void init(GameStage stage,int listPosition){
-        this.stage=stage;
-        this.listPosition=listPosition;
+
+    public void init(GameStage stage, int listPosition) {
+        this.stage = stage;
+        this.listPosition = listPosition;
         apply();
     }
-    public void apply(){}
-    public void act(float delta){
-       time-=1;
-        if(time<=0) {
+
+    public void apply() {
+    }
+
+    public void act(float delta) {
+        time -= 1;
+        if (time <= 0) {
             dispose();
         }
     }
-    public  void dispose(){
+
+    public void dispose() {
         stage.getPlayer().removeEffect(listPosition);
     }
 }
